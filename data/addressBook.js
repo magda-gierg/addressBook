d4ca945b10c5dd7ba5fef865b8e5a1d338662612
magda-gierg <mjgiergiczny@gmail.com>
2018-02-21 12:50:32 +1300

    create data

---
+++ data/addressBook.js
@@ -0,0 +1,38 @@
+const addressBook = {
+  Wellington: [
+    {
+      name: 'Magda',
+      address: '27 Cheshire St Wellington',
+      phone: '0226311279'
+    },
+    {
+      name: 'Sue',
+      address: '5 Pembroke St Wellington',
+      phone: '0226311345'
+    },
+    {
+      name: 'Ann',
+      address: '4 Willis St Wellington',
+      phone: '0226311789'
+    }
+  ],
+  Auckland: [
+    {
+      name: 'Tom',
+      address: '87a Richmond Rd Auckland',
+      phone: '0226398645'
+    },
+    {
+      name: 'Abby',
+      address: '8 Lincoln St Auckland',
+      phone: '0226398622'
+    },
+    {
+      name: 'Olivier',
+      address: '26 Vermont St Auckland',
+      phone: '0226398611'
+    }
+  ]
+}
+
+export default addressBook
